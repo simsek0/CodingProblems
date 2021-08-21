b6ba5260ea4c40e046927bf9ff87e6ba0c3da7d5
kurhanhakan <hakankurhan@yahoo.com>
2021-08-21 02:05:07 -0400

    Added some new Q&A

---
+++ src/DuplicatedChar.java
@@ -0,0 +1,35 @@
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DuplicatedChar {
+
+	
+	
+	//Write a Java Program to find the duplicate characters in a string.
+
+	public static void main(String[] args) {
+		
+		
+		
+		String sen = "Hello, this is me!";
+		char[] arr = sen.toCharArray();
+		char[] array;
+				
+		Set<Character> set = new HashSet<>();
+		
+ 		for(int i = 0; i < arr.length; i++) {
+ 			
+ 			
+ 			set.add(arr[i]);
+ 		}
+		
+		System.out.print(set);	
+		
+		
+		
+
+	}
+
+}
---
+++ src/FrequencyofChar.java
@@ -0,0 +1,32 @@
+
+
+public class FrequencyofChar {
+
+	//Write a Java program to find the frequency of character in a string.
+	
+	int frequency(String word) {
+		
+		char frequencyOfM = 'm';
+		int frequencyNumber = 0;
+		
+		for(int i = 0; i < word.length(); i++) 
+		{
+			if(frequencyOfM == word.charAt(i)) {
+				
+				++frequencyNumber;
+			}
+		}
+		
+		return frequencyNumber;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FrequencyofChar obj = new FrequencyofChar();
+		int freq = obj.frequency("Lorem Lipsum");
+		
+		System.out.println(freq);
+	}
+
+}
